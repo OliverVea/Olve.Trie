478a4b9e64f47b3407a51093fede633cbc80c1a4
Oliver Vea <oliver.vea@gmail.com>
2025-01-19 14:27:54 +0900

    feat: trie implements `ICollection<string>`

---
+++ Olve.Trie/Trie.ICollection.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+
+namespace Olve.Trie;
+
+public sealed partial class Trie : ICollection<string>
+{
+    public void Add(string item)
+    {
+        Add(item.AsSpan());
+    }
+
+    public bool Contains(string item) => Contains(item.AsSpan(), false);
+
+    public void CopyTo(string[] array, int arrayIndex)
+    {
+        foreach (var item in this)
+        {
+            array[arrayIndex++] = item;
+        }
+    }
+
+    public bool Remove(string item) => Remove(item.AsSpan());
+
+    public bool IsReadOnly => false;
+
+    public IEnumerator<string> GetEnumerator() => Words.GetEnumerator();
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+}
