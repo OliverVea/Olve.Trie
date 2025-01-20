41173ed2666edd25f275524d71330646fac0c3fd
Oliver Vea <oliver.vea@gmail.com>
2025-01-20 11:25:41 +0900

    test(profiling): adding project for profiling performance

---
+++ Olve.Trie.Profiling/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Runtime.CompilerServices;
+using Olve.Trie;
+using Olve.Trie.Profiling;
+
+var words = TestDataHelper.GetTestData()[..300_000];
+
+var trie = CreateTrie(words);
+var count = LookupInTrie(trie, words);
+
+Console.WriteLine(count);
+
+return;
+
+[MethodImpl(MethodImplOptions.NoInlining)]
+static Trie CreateTrie(string[] words)
+{
+    var trie = new Trie();
+
+    foreach (var word in words)
+    {
+        trie.Add(word);
+    }
+
+    return trie;
+}
+
+[MethodImpl(MethodImplOptions.NoInlining)]
+static int LookupInTrie(Trie trie, string[] words)
+{
+    var random = new Random(0);
+    var count = 0;
+
+    for (var i = 0; i < 200; i++)
+    {
+        var word = words[i];
+        var len = random.Next() % word.Length;
+        var prefix = word[.. len];
+
+        var results = trie.ListWithPrefix(prefix);
+
+        count += results.Count;
+    }
+
+
+    return count;
+}
+
---
+++ Olve.Trie.Profiling/TestDataHelper.cs
@@ -0,0 +1,17 @@
+﻿namespace Olve.Trie.Profiling;
+
+public static class TestDataHelper
+{
+    private const string TestDataPath = "TestData/words_alpha.txt";
+    private const int RandomSeed = 42;
+
+    public static string[] GetTestData(bool shuffle = true)
+    {
+        var words = File.ReadAllLines(TestDataPath);
+
+        var random = new Random(RandomSeed);
+        random.Shuffle(words);
+
+        return words;
+    }
+}
